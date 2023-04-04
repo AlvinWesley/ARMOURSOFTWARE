4586e154cdc85a72d3809c8bba86d054c925000b
ALVIN <wesealv@gmail.com>
2023-04-05 01:03:55 +0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer </title>
+    <link rel="stylesheet" href="headerfooterstyle.css">
+</head>
+<footer>
+    <div class="footerskeleton">
+        <div class="footercontents ">
+            <section class="left ">
+                <div class="objects " id="#objectleft">
+                    <h1>HEY ALSO FIND US ON </h1>
+                    <UL>
+                        <LI>FACEBOOK:armour</LI>
+                        <LI>TWITTER:@armour</LI>
+                        <LI>INSTAGRAM:armour</LI>
+                        <LI>LINKED IN:@armour</LI>
+                        <LI>WEBSITE: WWW.armour.COM</LI>
+                        <LI>EMAIL US :armour@gmail.com</LI>
+                    </UL>  
+                </div>
+            </section><hr>
+            <section class="center ">
+                <div class="objects "id="#objectcenter">
+                    <h1>Call us now!!</h1>
+                <ul>
+                    <li>phone one:+254743617539</li>
+                    <li>phone two:+254795628991</li>
+                    <li>fax:+11191010</li>
+                    <li>whatsapp:+254743617539</li>
+                    <li>telegram:+254743617539</li>
+                </ul>
+                </div>
+            </section><hr>
+            <section class="right ">
+                <div class="objects "id="#objectright">
+                    <h1>SOMETHING ABOUT US</h1>
+                    <!-- <P>We are a 24hr running shop located on harry street opposite Kitlon bank on jambo highway 
+                        we offer exquisite services for you and your coffee and allow you to sit back and relax take your mind off the world a bit </P>
+                         -->
+                        <ul>
+                            <h2>SERVICES AVAILABLE</h2>
+                    <LI>accounting system</LI>
+                    <li>inventory management system</li>
+                    <li>library management system</li>
+                    <li>students management system </li>
+                    <li>examination analysis system </li>
+                </ul>
+                </div>
+            </section>
+
+
+        </div>
+    </div>
+</footer>
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HEADER</title>
+    <!-- <link rel="stylesheet" href="headerfootermediasettings.css"> -->
+    <link rel="stylesheet" href="headerfooterstyle.css">
+    
+</head>
+<header>
+<div class="headerskeleton ">
+    <div class="headerupper ">
+<div class="left ">
+    <DIV class="armourlogo ">
+
+    </DIV>
+</div>
+<div class="right ">
+    <ul>
+        <li><a href="#guest.html"  id="guest">Guest</a></li>
+        <LI><a href="login.html" id="login">Log In</a></LI>
+    </ul>
+</div>
+    </div>
+    <div class="headerlower ">
+        <div class="lowercontents ">
+            <section class="about "><a href="#">About</a></section>
+            <section class="services "><a href="#">Services</a></section>
+            <section class="help "><a href="#">Help!</a></section>
+            <section class="products "><a href="#">Products</a></section>
+        </div>
+    </div>
+</div>
+</header>
+</html>
